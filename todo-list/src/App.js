0efc1279556ac5bfc05b14538b22a56a9f5b826f
Chamy619 <yd95069506@naver.com>
2021-04-27 23:19:27 +0900

    템플릿 생성

---
+++ todo-list/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import TodoListTemplate from './components/TodoListTemplate.js';
+
+class App extends React.Component {
+  render() {
+    return (
+      <TodoListTemplate>
+        템플릿 완성
+      </TodoListTemplate>
+    );
+  }
+}
+
+export default App;
